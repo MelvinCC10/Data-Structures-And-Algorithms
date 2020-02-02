7450b4ca1d520560831d9dfb389d70e0bc7dc8d0
Reven <Melvin.cordon21@gmail.com>
2020-02-01 22:12:25 -0600

    insert method added to linked list classes

--- LinkedListPython.py
+++ LinkedListPython.py
@@ -24,6 +24,16 @@ class LinkedList():
             self.tail = self.tail.next
             len += 1
 
+    def insert(self, data, node1, node2):
+        """ Inserts a node between to other nodes """
+        traverse_point = self.head
+        while traverse_point.data != node1 and traverse_point.next.data != node2:
+            traverse_point = traverse_point.next
+
+        new_node = Node(data)
+        new_node.next = traverse_point.next
+        traverse_point.next = new_node
+
 class DoublyNode():
 
     def __init__(self, data, next = None, prev = None):
@@ -50,7 +60,21 @@ class DoublyLinkedList():
             self.tail = self.tail.next
             self.len += 1
 
+    def insert(self, data, node1, node2):
+        """ Inserts a node between to other nodes """
+        traverse_point = self.head
+        while traverse_point.data != node1 and traverse_point.next.data != node2:
+            traverse_point = traverse_point.next
 
+        new_node = Node(data)
+
+        # Setting new nodes next and prev to node1 and node2 respectivly
+        new_node.next = traverse_point.next
+        new_node.prev = traverse_point
+
+        # Setting the next and prev of node1 and node2 respectivly to new_node
+        traverse_point.next.prev = new_node
+        traverse_point.next = new_node
 
 
 """ Example """
@@ -59,6 +83,9 @@ class DoublyLinkedList():
 linked_list = DoublyLinkedList()
 for i in range(10):
     linked_list.append(i)
+
+# Insert a 22  between 3 and 4
+linked_list.insert(22,3,4)
 
 # Traverse Forward
 traverse_point = linked_list.head
